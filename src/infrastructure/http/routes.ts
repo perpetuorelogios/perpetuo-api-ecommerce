4623085d7c1f60822e42771832a6b1ba71df7434
João Vitor Camargo <joaocamargo06@gmail.com>
2026-02-05 18:22:21 -0300

    Ajust deploy

--- src/infrastructure/http/routes.ts
+++ src/infrastructure/http/routes.ts
@@ -11,7 +11,7 @@ export async function routes(app: FastifyInstance) {
   app.get('/health', async (req, reply) => {
     const uptime = Math.floor((Date.now() - startTime) / 1000)
     const response = {
-      status: 'ok ok ok maoe',
+      status: 'ok',
       timestamp: new Date().toISOString(),
       uptime,
       version: '1.0.0',
