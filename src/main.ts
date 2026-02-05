7c471e4e9ccd8881003eb2f4b89523a0a493aec6
João Vitor Camargo <joaocamargo06@gmail.com>
2026-02-04 23:32:20 -0300

    Adicionar host ao método listen da aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { app } from './infrastructure/http/server'
 
 const port = Number(process.env.PORT) || 3000
 
-app.listen({ port }, () => {
+app.listen({ port, host: '0.0.0.0' }, () => {
   console.log(`🚀 API running on ${port}`)
 })
